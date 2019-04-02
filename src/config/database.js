de79d9c2d2fe8edea7d1e6673d34e36567220392
Felipe Alves de Jesus <fajreyy.bh@gmail.com>
2019-04-02 18:23:06 -0300

    commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: '127.0.0.1',
-  username: 'docker',
-  password: 'docker',
+  // host: '127.0.0.1',
+  host: 'gonodemodulo2.ck4kkuslchyx.sa-east-1.rds.amazonaws.com',
+  username: 'FajReyy',
+  password: 'Faj82469173reyy',
   database: 'gonodemodulo2',
   operatorAliases: false,
   define: {
